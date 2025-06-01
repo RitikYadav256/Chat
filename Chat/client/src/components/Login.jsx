c0387ea72676426e0e34457e0c6968aca2798fe1
RitikYadav256 <118496635+RitikYadav256@users.noreply.github.com>
2025-06-01 14:42:54 +0530

    Update Login.jsx

--- Chat/client/src/components/Login.jsx
+++ Chat/client/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     setSuccessMessage('');
 
     try {
-      const res = await fetch('http://localhost:4000/login', {
+      const res = await fetch('https://chat-nlss.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
